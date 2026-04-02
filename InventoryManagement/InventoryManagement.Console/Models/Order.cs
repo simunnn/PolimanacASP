b69089f2d97756d1d907e85c948dfd02eef98781
Polimanac <spolimana@tvz.hr>
2026-04-02 17:05:36 +0200

    Added order and order item classes

---
+++ InventoryManagement/InventoryManagement.Console/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryManagement.Console.Enums;
+
+namespace InventoryManagement.Console.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public string Note { get; set; }
+
+        public User User { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Console/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InventoryManagement.Console.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal Discount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}

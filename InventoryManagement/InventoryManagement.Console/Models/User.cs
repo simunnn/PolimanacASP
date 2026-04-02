e7fcab10f0a43ab2e56ba4bfd96cf6e8a46a48fd
Polimanac <spolimana@tvz.hr>
2026-04-02 17:03:10 +0200

    Added core inventory model classes

---
+++ InventoryManagement/InventoryManagement.Console/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryManagement.Console.Enums;
+
+namespace InventoryManagement.Console.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+        public User()
+        {
+            Orders = new List<Order>();
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Console/Models/Warehouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryManagement.Console.Enums;
+
+namespace InventoryManagement.Console.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Capacity { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Manager { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public bool IsActive { get; set; }
+        public WarehouseType Type { get; set; }
+
+        public List<InventoryItem> InventoryItems { get; set; }
+
+        public Warehouse()
+        {
+            InventoryItems = new List<InventoryItem>();
+        }
+    }
+}

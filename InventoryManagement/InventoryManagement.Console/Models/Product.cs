4621014097d13ccd52af51ae062c96ad8cf3bbe6
Polimanac <spolimana@tvz.hr>
2026-04-02 16:59:20 +0200

    Added enums and core product domain classes

---
+++ InventoryManagement/InventoryManagement.Console/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagement.Console.Enums;
+
+namespace InventoryManagement.Console.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public int MinimumStock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public ProductType Type { get; set; }
+
+        public Category Category { get; set; }
+        public Supplier Supplier { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+        public List<InventoryItem> InventoryItems { get; set; }
+
+        public Product()
+        {
+            OrderItems = new List<OrderItem>();
+            InventoryItems = new List<InventoryItem>();
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Console/Models/Supplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Console.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ContactPerson { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Supplier()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
